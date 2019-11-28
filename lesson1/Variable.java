7aa5117329b3a7d6314ef172f4b854433cf0e78c
unknown <D:\\The Bat!>
2019-11-28 22:07:35 +0300

    First java lesson

---
+++ lesson1/Variable.java
@@ -0,0 +1,22 @@
+public class Variable {	
+    public static void main(String[] args) {
+
+        int a = 16;
+        byte b = 8;
+        short s = 8;
+        long l = 930;
+        float f = 1.8f;
+        double d = 254.5858d;
+        char c = 'D';
+        boolean boo = true;
+
+        System.out.println("RAM " + a +" GB");
+        System.out.println("Processor " + b +" cores");
+        System.out.println("Processor intel " + s +" th generation");
+        System.out.println("Video card model = " + l +" MX");
+        System.out.println("Processor frequancy " + f +" GHz");
+        System.out.println("Random double number " + d);
+        System.out.println("Java installed on " + c +" local drive");
+        System.out.println("The Moscow is a capital of Russia, and it is " + boo);
+    }
+}
