2505f5764576adfb3028df6bcb593bba40a943b5
unknown <D:\\The Bat!>
2019-12-06 02:40:16 +0300

    Добавлена проверка на продолжение Да/Нет

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -1,35 +1,58 @@
-import java.util.Scanner;
-
 public class Calculator {
-    public static void main(String[] args) {
-        System.out.println("Введите любое целое число a");
-        Scanner scanNumbers = new Scanner(System.in);
-        int numberA = scanNumbers.nextInt();
-        System.out.println("Введите любое целое число b");
-        int numberB = scanNumbers.nextInt();
-        scanNumbers.nextLine();
-        System.out.println("Введите одно из действий +, -, *, /, ^, %");
-        String mathSymbol = scanNumbers.nextLine(); 
-        char operator = mathSymbol.charAt(0);
+    public int numberA;
+    public int numberB;
+    public char operator;
+    public boolean isContinue;
+    public String answer;
 
-        if (operator == '+') {
-            System.out.println("Сумма числа a и числа b = " + (numberA + numberB));
-        } else if (operator == '-') {
-            System.out.println("Разница числа a и числа b = " + (numberA - numberB));
-        } else if (operator == '/') {
-            System.out.println("Частное числа a и числа b = " + (numberA / numberB));
-        } else if (operator == '*') {
-            System.out.println("Произведение числа a и числа b = " + (numberA * numberB));
-        } else if (operator == '^') {
-            int result = 1;
-            for (int i = 1; i <= numberB; i++ ) {
-                result *= numberA;
-            }
-            System.out.println("Число а в степени b = " + result);
-        } else if (operator == '%') {
-            System.out.println("Остаток от деления числа a на число b = " + (numberA % numberB));
-        } else {
-            System.out.println("Неверный оператор");
+    public int useCalculate(int numberA, int numberB, char operator) {
+        int result = 1;
+
+        switch (operator) {
+            case '+':
+                result = numberA + numberB;
+                break;
+            case '-':
+                result = numberA - numberB;
+                break;
+            case '/':
+                result = numberA / numberB;
+                break;
+            case '*':
+                result = numberA * numberB;
+                break;
+            case '^':
+                for (int i = 1; i <= numberB; i++) {
+                    result *= numberA;
+                }
+                break;
+            case '%':
+                result = numberA % numberB;
+                break;
+            default:
+                System.out.println("Неверный оператор");
+                break;
+        }
+        return result;
+    }
+
+    public void Continue() {
+        switch (answer) {
+            case "Да":
+                isContinue = true;
+                break;
+            case "Нет":
+                isContinue = false;
+                break;
+            default:
+                System.out.println("Хотите продолжить? [да/нет]:");
+                isContinue = true;
+                break;
         }
     }
 }
+
+
+
+
+
--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -4,15 +4,21 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator simple = new Calculator();
         Scanner scanNumbers = new Scanner(System.in);
-        System.out.println("Введите первое число:");
-        simple.numberA = scanNumbers.nextInt();
-        scanNumbers.nextLine();
-        System.out.println("Введите знак математической операции:");
-        String mathSymbol = scanNumbers.nextLine();
-        simple.operator = mathSymbol.charAt(0);
-        System.out.println("Введите второе число:");
-        simple.numberB = scanNumbers.nextInt();
 
-        System.out.println("Результат вычисления " + simple.useCalculate(simple.numberA, simple.numberB, simple.operator));
+        do {
+            System.out.println("Введите первое число:");
+            simple.numberA = scanNumbers.nextInt();
+            scanNumbers.nextLine();
+            System.out.println("Введите знак математической операции:");
+            String mathSymbol = scanNumbers.nextLine();
+            simple.operator = mathSymbol.charAt(0);
+            System.out.println("Введите второе число:");
+            simple.numberB = scanNumbers.nextInt();
+            scanNumbers.nextLine();
+            System.out.println("Результат вычисления " + simple.useCalculate(simple.numberA, simple.numberB, simple.operator));
+            System.out.println("Хотите продолжить? [да/нет]: ");
+            simple.answer = scanNumbers.nextLine();
+            simple.Continue();
+        } while (simple.isContinue);
     }
 }
