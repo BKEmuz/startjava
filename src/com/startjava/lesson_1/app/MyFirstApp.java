89b855d9be3d2431843c73cb6760a71fc7fb8e40
unknown <D:\\The Bat!>
2020-03-10 10:27:13 +0300

    Fixed folders structure

--- src/com/startjava/lesson_1/app/MyFirstApp.java
+++ src/com/startjava/lesson_1/app/MyFirstApp.java
@@ -1,4 +1,4 @@
-package src.com.startjava.lesson_1.app;
+package com.startjava.lesson_1.app;
 public class MyFirstApp {   
     public static void main(String[] args) {
         System.out.println("Hello, world");
